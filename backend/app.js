0569ebd8a1c3c8a21734f1f06c8d20a2219a9515
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-06-17 14:08:37 +0200

    Add show search input on request

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send("Hello World");
+	const searchParam = req.query.search;
+	res.send(`Your search: ${searchParam}`); 
 });
 
 app.listen(8000, () => {
